ba16cbcf609ee94b9eb8c7f03fa62ae44e416f49
HB <85717438+HBAMAL@users.noreply.github.com>
2021-07-21 12:29:54 +0530

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -30,9 +30,9 @@ async def UploadVideo(bot: Client, cb: CallbackQuery, merged_vid_path: str, widt
                 ),
                 reply_markup=InlineKeyboardMarkup(
                     [
-                        [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")],
-                        [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo"),
-                         InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]
+                        [InlineKeyboardButton("📢CHANNEL📢", url="https://t.me/telsabots")],
+                        [InlineKeyboardButton("🎬Group🎬", url="https://t.me/filimsmovie"),
+                         InlineKeyboardButton("🧑🏼‍💻DEV🧑🏼‍💻", url="https://t.me/alluaddict")]
                     ]
                 )
             )
@@ -51,9 +51,9 @@ async def UploadVideo(bot: Client, cb: CallbackQuery, merged_vid_path: str, widt
                 ),
                 reply_markup=InlineKeyboardMarkup(
                     [
-                        [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")],
-                        [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo"),
-                         InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]
+                        [InlineKeyboardButton("📢CHANNEL📢", url="https://t.me/telsabots")],
+                        [InlineKeyboardButton("🎬Group🎬", url="https://t.me/filimsmovie"),
+                         InlineKeyboardButton("🧑🏼‍💻DEV🧑🏼‍💻", url="https://t.me/alluaddict")]
                     ]
                 )
             )
